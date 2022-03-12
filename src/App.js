eeda22ba68fb7a631f9504d6feaba8b20be01cde
CeloRocha <marcelo.ds.rocha77@gmail.com>
2022-03-12 10:11:17 -0300

    Profile card update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+
+import {useState} from 'react';
 import Homepage from './pages/home/Homepage';
 function App() {
+
+  const [ lightMode, setLightMode ] = useState(false)
+
   return (
     <div className="App">
-      <Homepage />
+      <button onClick={()=>setLightMode(prev => !prev)}>Toogle</button>
+      <Homepage lightMode={lightMode}/>
     </div>
   );
 }
--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -8,10 +8,10 @@ import githubImg from '../../assets/GitHub.svg';
 import instagramImg from '../../assets/Instagram.svg';
 import linkedinImg from '../../assets/Linkedin.svg'
 
-const Homepage = () => {
+const Homepage = (props) => {
   return (
     <div>
-        <div className='profile-card'>
+        <div className={`profile-card ${props?.lightMode ? 'light' : ''} ${props.className}`}>
             <div className='profile-img'>
                 <img src={marceloImg} alt="Marcelo Rocha"/>
             </div>
@@ -19,20 +19,29 @@ const Homepage = () => {
             <h2>Frontend Developer</h2>
             <span>marcelorocha.website</span>
             <button className='profile-email-button'>
-                <img src={emailImg} alt="" />
+                <div className='email-icon'></div>
+                {/* <img src={emailImg} alt="" /> */}
                 <span>Email</span>
             </button>
-            <section className="profile-info">
+            {/* <section className="profile-info">
                 <h3>About</h3>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, tenetur. Vel quos explicabo iusto! Dolorem eligendi, nisi excepturi tempora culpa fugiat, hic asperiores aliquam mollitia vero animi, a numquam saepe?</p>
                 <h3>Interests</h3>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt id atque pariatur laboriosam, animi tenetur suscipit. Odit molestias dolores blanditiis iure, reprehenderit odio debitis, repellendus, dolore eius deleniti voluptas laborum!</p>
-            </section>
+            </section> */}
             <div className='profile-footer'>
-                <img src={facebookImg} alt="Facebook" />
-                <img src={instagramImg} alt="Instagram" />
-                <img src={linkedinImg} alt="Linkedin" />
-                <img src={githubImg} alt="" />
+                <a href="https://www.facebook.com/marcelo.rocha.33671" target='_blank'>
+                    <img src={facebookImg} alt="Facebook" />
+                </a>
+                <a href="https://www.instagram.com/marcelo.ds.rocha/" target='_blank'>
+                    <img src={instagramImg} alt="Instagram" />
+                </a>
+                <a href="https://www.linkedin.com/in/marcelo-rocha-091b551bb/" target='_blank'>
+                    <img src={linkedinImg} alt="Linkedin" />
+                </a>
+                <a href="https://github.com/CeloRocha" target='_blank'>
+                    <img src={githubImg} alt="GitHub" />
+                </a>
             </div>
         </div>
     </div>
