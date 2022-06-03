9468ec45bd45bae561433e8719703a3a1ab80356
Marcelo Rocha <marcelo.ds.rocha77@gmail.com>
2022-06-02 21:23:30 -0300

    feat: 🎸 New experience added

--- src/helper/experiences.js
+++ src/helper/experiences.js
@@ -1,4 +1,9 @@
-const experiences = [
+const experiences = [{
+        name: 'Self Solutions',
+        job: 'Estágio em Desenvolvimento Web',
+        description: 'Estágio para desenvolvimento de aplicações com foco no Front-end utilizando ReactJS. Trabalhando no desenvolvimento de uma aplicação de monitoramento em tempo real, utilizando mapas que visa a emissão de alertas de risco para empresas de acordo com as condições metereológicas.',
+        date: 'Maio, 2022 - Hoje'
+    },
     {
         name: 'Black Bee Drones',
         job: 'Software Developer',
@@ -8,7 +13,7 @@ const experiences = [
     {
         name: 'Universidade Federal de Itajubá - Minas Gerais',
         job: '  Engenharia de Controle e Automação',
-        date: '2018 - Today'
+        date: '2018 - Hoje'
     },
     {
         name: 'Colégio Técnico Industrial de Guaratinguetá - CTIG',
